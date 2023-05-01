f9b1f41fc425a5f1dd5dd8eb9fde16225334e1af
unknown <ecblickenderfer@gmail.com>
2023-04-30 23:33:38 -0400

    add readme image

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # weatherapp
 
+![Weather App Homepage](./assets/images/weatherapp.PNG)
+
 ## Overview
 
 This application is intended for travelers to be able to view the local weather and five-day forecast of a given city. Users may type a particular city into the search bar and are presented with the forecast. The application also keeps a user's history visible in case they intend to revisit a particular city and are in need of the forecast. 
--- script.js
+++ script.js
@@ -14,14 +14,14 @@ function getWeather(city) {
             console.log(data);
             var lat = data.coord.lat;
             var lon = data.coord.lon;
-            var pic = data.weather[0].icon; 
+            var pic = data.weather[0].icon;
             getForecast(lat, lon);
             document.querySelector(".city-name").textContent = data.name;
             document.querySelector(".temp").textContent = "Temp: " + data.main.temp + "F";
-            document.querySelector(".wind").textContent = "Wind: " + data.wind.speed + "MPH"; 
+            document.querySelector(".wind").textContent = "Wind: " + data.wind.speed + "MPH";
             document.querySelector(".humidity").textContent = "Humidity: " + data.main.humidity + "%";
-            document.querySelector("#weather-icon").setAttribute("src", `http://openweathermap.org/img/wn/${pic}@2x.png`); 
-    
+            document.querySelector("#weather-icon").setAttribute("src", `http://openweathermap.org/img/wn/${pic}@2x.png`);
+
         })
 }
 
@@ -41,14 +41,14 @@ function getForecast(lat, lon) {
                     forecastCard.setAttribute("class", "forecast-card")
                     var cardBody = document.createElement("div")
                     var date = document.createElement("h5")
-                    var pic = data.list[i].weather[0].icon; 
+                    var pic = data.list[i].weather[0].icon;
                     date.textContent = element.dt_txt.split(" ")[0]
                     cardBody.append(date);
                     forecastCard.append(cardBody);
-                    
-                    document.querySelector(".forecast-grid").append(forecastCard); 
+
+                    document.querySelector(".forecast-grid").append(forecastCard);
                     console.log(data.list[0].main.temp);
-                    
+
                     var temp = document.createElement("p");
                     temp.textContent = data.list[i].main.temp;
                     cardBody.append(temp);
@@ -72,37 +72,27 @@ function getForecast(lat, lon) {
 }
 
 
-//event listener, target will match class of city, 
-//call first function to pass the city name 
-searchButton.addEventListener("click", function() {
-    // event.preventDefault()
-    // document.querySelector(".local-forecast").classList.remove("hidden");
-    // document.querySelector(".forecast-grid").classList.remove("hidden");
-    //use above line to hide introduction message 
+searchButton.addEventListener("click", function () {
     var cityName = document.querySelector(".search").value
     getWeather(cityName);
     const array = JSON.parse(localStorage.getItem("searchHistory")) || [];
-    if (!array.includes(cityName)){
+    if (!array.includes(cityName)) {
         array.push(cityName);
     }
     localStorage.setItem("searchHistory", JSON.stringify(array));
-
 });
 
 var searchBar = document.querySelector(".searchbar");
 
-function renderCityHistory(){
+function renderCityHistory() {
     var cityHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
     console.log(cityHistory);
-    for (i = 0; i < cityHistory.length; i++){
+    for (i = 0; i < cityHistory.length; i++) {
         var cityButton = document.createElement("button");
-        cityButton.setAttribute("class", "city-btn"); 
+        cityButton.setAttribute("class", "city-btn");
         cityButton.textContent = cityHistory[i];
         cityButton.setAttribute("onclick", `getWeather('${cityHistory[i]}')`);
-        // cityButton.addEventListener("click", getWeather(cityHistory[i]));
         searchBar.appendChild(cityButton);
-        
     }
 }
-
 renderCityHistory();
